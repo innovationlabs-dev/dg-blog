bae8993ab608ad857dc9f9e1f487476a43450a00
Taofeeq Hamzat <hamzatdestiny@gmail.com>
2025-04-20 21:33:09 +0100

    feat: Customise components, assets, and site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         sizes="16x16"
         href={`${basePath}/static/favicons/favicon-16x16.png`}
       />
+      <link rel="icon" type="image/png" href={`${basePath}/static/favicons/favicon.png`} />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
       <link
         rel="mask-icon"
